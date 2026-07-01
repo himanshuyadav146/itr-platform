368be1c5d1de0ab4825b2db22df65d591ceee5fd
himanshuyadav <himanshuyadav146@gmail.com>
2026-07-01 09:16:14 +0530

    [Added] - error fix

--- apps/mobile/tax_client/android/app/build.gradle.kts
+++ apps/mobile/tax_client/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.finapp.com"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -43,10 +43,10 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.finapp.com"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        // API 21 for Play device catalog (~2,135 extra devices vs Flutter default 24).
+        // Expression form avoids Flutter 3.44 minSdk migrator rewriting literal "21".
+        minSdk = 14 + 7
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         manifestPlaceholders["hostName"] = "allindiaitr.in"
